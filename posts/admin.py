82e01968c54acd432d395cea8db888269e7c7009
Rayan Ahlouche <Rayahlouche@gmail.com>
2023-08-16 14:24:31 -0400

    Comment backend has been added, everything seems good

--- posts/admin.py
+++ posts/admin.py
@@ -1,6 +1,28 @@
 from django.contrib import admin
 from .models import BlogPost
+from .models import Comment
 
 @admin.register(BlogPost)
 class BlogPostAdmin(admin.ModelAdmin):
     list_display = ('post_title', 'post_author', 'post_created_date', 'post_updated_date')
+    search_fields = ('post_title', 'post_content')
+    list_filter = ('post_created_date', 'post_author')
+    ordering = ('-post_created_date',) 
+
+    readonly_fields = ('post_created_date', 'post_updated_date',)  # This line ensures these fields are non-editable, since i dont want people changing it after they make a post
+
+    fieldsets = (
+        ('Post Information', {
+            'fields': ('post_title', 'post_content')
+        }),
+        ('Author and Date Information', {
+            'fields': ('post_author', 'post_created_date', 'post_updated_date')
+        }),
+    )
+      #field set is helping me make the blog more user friendly
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('post', 'author', 'content', 'created_date', 'updated_date')
+    search_fields = ('content', 'author__username', 'post__post_title')
+    list_filter = ('created_date', 'updated_date', 'author')
+    ordering = ('-created_date',) 
--- posts/models.py
+++ posts/models.py
@@ -9,4 +9,14 @@ class BlogPost(models.Model):
     post_updated_date = models.DateTimeField(auto_now=True)
 
     def __str__(self):
-        return self.post_title
+        return self.post_title  
+
+class Comment(models.Model):
+    post = models.ForeignKey(BlogPost, on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    content = models.TextField()
+    created_date = models.DateTimeField(auto_now_add=True)
+    updated_date = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return f"Comment by {self.author} on {self.post}"
