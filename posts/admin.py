6cef7c3e2fac2d0232cd6e032dd29f165bbbc50d
Rayan Ahlouche <Rayahlouche@gmail.com>
2023-08-16 13:13:19 -0400

    Added the admin interface, welcome page is not there

--- posts/admin.py
+++ posts/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import BlogPost
 
-# Register your models here.
+@admin.register(BlogPost)
+class BlogPostAdmin(admin.ModelAdmin):
+    list_display = ('post_title', 'post_author', 'post_created_date', 'post_updated_date')
